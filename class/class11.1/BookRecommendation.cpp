fb9aa620dce43110e871d7a01013b54bf71a5738
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2024-07-22 02:08:35 +0300

    fix println task11.1

--- class/class11.1/BookRecommendation.cpp
+++ class/class11.1/BookRecommendation.cpp
@@ -25,16 +25,16 @@ int main() {
     std::shuffle(books.begin(), books.end(), g);
 
     for (const auto& book : books) {
-        std::println << "We recommend: " << book << std::endl;
-        std::println << "Do you like this book? (yes/no): ";
+        std::println("We recommend: {}", book);
+        std::println("Do you like this book? (yes/no): ");
         std::string response;
         std::cin >> response;
         if (response == "yes") {
-            std::println << "Enjoy your book!" << std::endl;
+            std::println("Enjoy your book!");
             return 0;
         }
     }
 
-    std::println << "Adios amigo!" << std::endl;
+    std::println("Adios amigo!");
     return 0;
 }
